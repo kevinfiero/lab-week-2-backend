cfb43cf43cfe1c13b7b096c14ac741347986a8b4
kevinfiero <kjf46@cornell.edu>
2020-11-02 23:47:46 -0800

    urls for images

--- data/coffees.js
+++ data/coffees.js
@@ -2,7 +2,7 @@ module.exports = [
   {
       name_id: 'french-roast-12oz',
       name: 'French Roast',
-      image: 'french-roast.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1695/large/media.nl?1524324908',
       description: '12oz French Roast',
       category: 'Organic',
       price: 16
@@ -10,7 +10,7 @@ module.exports = [
   {
       name_id: 'hair-bender-12oz',
       name: 'Hair Bender',
-      image: 'hair-bender.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1692/large/media.nl?1524324843',
       description: '12oz Hair Bender',
       category: 'Organic',
       price: 15
@@ -18,7 +18,7 @@ module.exports = [
   {
       name_id: 'holler-mountatin-12oz',
       name: 'Holler Mountain',
-      image: 'holler-mountain.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1691/large/media.nl?1524324733',
       description: '12oz Holler Mountain',
       category: 'Organic',
       price: 16
@@ -26,7 +26,7 @@ module.exports = [
   {
       name_id: 'house-blend-12oz',
       name: 'House Blend',
-      image: 'house-blend.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1740/large/media.nl?1532727765',
       description: '12oz House Blend',
       category: 'Organic',
       price: 15
@@ -34,7 +34,7 @@ module.exports = [
   {
       name_id: 'trapper-creek-12oz',
       name: 'Trapper Creek',
-      image: 'trapper-creek.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1687/large/media.nl?1524324582',
       description: '12oz Trapper Creek',
       category: 'Organic',
       price: 16
@@ -42,7 +42,7 @@ module.exports = [
   {
       name_id: 'el-puente-12oz',
       name: 'Honduras El Puente',
-      image: 'el-puente.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1912/large/puente_sm.png?1591641424',
       description: '12oz Honduras El Puente',
       category: 'Single Origin Organic',
       price: 20
@@ -50,23 +50,23 @@ module.exports = [
   {
       name_id: 'el-injerto-bourbon-12oz',
       name: 'Guatemala El Injerto Bourbon',
-      image: 'el-injerto-bourbon.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1908/large/InjertoBourbon_2020.png?1590612462',
       description: '12oz Guatemala El Injerto Bourbon',
       category: 'Single Origin Organic',
       price: 20
   },
   {
-      name_id: 'marvin-robles-12oz',
-      name: 'Costa Rica Marvin Robles',
-      image: 'marvin-robles.png',
-      description: '12oz Costa Rica Marvin Robles',
+      name_id: 'montes-de-oro-12oz',
+      name: 'Costa Rica Montes De Oro',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1901/large/MontesDeOro_2020.png?1590700066',
+      description: '12oz Costa Rica Montes De Oro',
       category: 'Single Origin Organic',
       price: 22
   },
   {
       name_id: 'bies-penantan-12oz',
       name: 'Indonesia Bies Penatan',
-      image: 'bies-penantan.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1913/large/Indo_Bies2020.png?1586194787',
       description: '12oz Indonesia Bies Penatan',
       category: 'Single Origin Organic',
       price: 19
@@ -74,7 +74,7 @@ module.exports = [
   {
       name_id: 'ndaroini-12oz',
       name: 'Kenya Ndaroini',
-      image: 'ndaroini.png',
+      image: 'https://d1rusy4hxccwbq.cloudfront.net/spree/images/1985/large/media.nl?1600101766',
       description: '12oz Kenya Ndaroini',
       category: 'Single Origin Organic',
       price: 19
